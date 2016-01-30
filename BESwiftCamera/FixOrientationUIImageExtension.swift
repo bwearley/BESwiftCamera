9623cee44d3cf2448df74bb116108149d2c4769c
Brian Earley <bwearley@gmail.com>
2016-01-30 12:44:03 -0500

    Edits to FixOrientation method

--- BESwiftCamera/FixOrientationUIImageExtension.swift
+++ BESwiftCamera/FixOrientationUIImageExtension.swift
@@ -22,37 +22,31 @@ extension UIImage {
 		var transform = CGAffineTransformIdentity
 		
 		switch self.imageOrientation {
-			case .Down: break
-			case .DownMirrored:
+			case .Down, .DownMirrored:
 				transform = CGAffineTransformTranslate(transform, self.size.width, self.size.height)
 				transform = CGAffineTransformRotate(transform, π)
-			case .Left: break
-			case .LeftMirrored:
+                break
+			case .Left, .LeftMirrored:
 				transform = CGAffineTransformTranslate(transform, self.size.width, 0)
 				transform = CGAffineTransformRotate(transform, π_2)
-			case .Right: break
-			case .RightMirrored:
+                break
+			case .Right, .RightMirrored:
 				transform = CGAffineTransformTranslate(transform, 0, self.size.height)
 				transform = CGAffineTransformRotate(transform, -π_2)
-			case .Up: break
-			case .UpMirrored: break
-			//default:
+                break
+			case .Up, .UpMirrored: break
 		}
 		
 		switch self.imageOrientation {
-			case .UpMirrored: break
-			case .DownMirrored:
+			case .UpMirrored, .DownMirrored:
 				transform = CGAffineTransformTranslate(transform, self.size.width, 0)
 				transform = CGAffineTransformScale(transform, -1, 1)
-			case .LeftMirrored: break
-			case .RightMirrored:
+                break
+			case .LeftMirrored, .RightMirrored:
 				transform = CGAffineTransformTranslate(transform, self.size.height, 0)
 				transform = CGAffineTransformScale(transform, -1, 1)
-			case .Up: break
-			case .Down: break
-			case .Left: break
-			case .Right: break
-			//default:
+                break
+			case .Up, .Down, .Left, .Right: break
 		}
 		
 		// Now we draw the underlying CGImage into a new context, applying the transform calculated above.
@@ -63,22 +57,24 @@ extension UIImage {
             CGImageGetBitsPerComponent(self.CGImage),
             0,
             CGImageGetColorSpace(self.CGImage),
-            CGImageGetBitmapInfo(self.CGImage).rawValue)
+            CGImageAlphaInfo.PremultipliedLast.rawValue)
+            //CGImageGetBitmapInfo(self.CGImage).rawValue)
 
 		CGContextConcatCTM(ctx, transform)
 		switch self.imageOrientation {
-			case .Left: break
-			case .LeftMirrored: break
-			case .Right: break
-			case .RightMirrored:
-				// Grr...
+			case .Left, .LeftMirrored, .Right,.RightMirrored:
 				CGContextDrawImage(ctx, CGRectMake(0,0,self.size.height,self.size.width), self.CGImage)
+                break
 			default:
 				CGContextDrawImage(ctx, CGRectMake(0,0,self.size.width,self.size.height), self.CGImage)
+                break
 		}
 		
 		// And now we just create a new UIImage from the drawing context
-		let cgimg = CGBitmapContextCreateImage(ctx)
+        //let cgImage: CGImageRef = CGBitmapContextCreateImage(ctx)!
+        //return UIImage(CGImage: cgImage)
+        
+        let cgimg = CGBitmapContextCreateImage(ctx)
         return UIImage(CGImage:cgimg!)
 	}
 }
